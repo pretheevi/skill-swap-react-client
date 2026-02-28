3c8ed2922df2155d4c6250f64765fbb0b4b128bc
pretheevi <pretheeviraj0805@gmail.com>
2026-02-28 22:28:36 +0530

    api update

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-const API  = axios.create({
-  baseURL:  "http://localhost:8080/api",
+const API = axios.create({
+  baseURL: "https://insta-mirror-server.onrender.com/api",
 });
 
 // request interceptor to add token to headers
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if(token) {
+  if (token) {
     config.headers["Authorization"] = `Bearer ${token}`;
   }
   return config;
-})
+});
 
 export default API;
 
