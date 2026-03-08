a0c87bd054b0183720bad25ae1d163778ea11e87
pretheevi <pretheeviraj0805@gmail.com>
2026-03-08 18:48:18 +0530

    update server http

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://insta-mirror-server.onrender.com/api",
 });
 
 // request interceptor to add token to headers
